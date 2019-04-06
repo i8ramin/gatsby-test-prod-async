987736b863a2acafbdbc5095c786c4d76a660fba
Ramin B <ramin@wework.com>
2019-04-05 23:12:35 -0400

    use fake api

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const ipstackToken = process.env.GATSBY_IPSTACK_TOKEN
-
 export default () => {
   const [geoData, setGeoData] = useState({})
 
   useEffect(() => {
     async function fetchData() {
-      const resp = await axios.get(`http://api.ipstack.com/check?access_key=${ipstackToken}`)
+      const resp = await axios.get(`https://jsonplaceholder.typicode.com/todos/1`)
       setGeoData(resp.data)
     }
 
